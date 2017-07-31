fe1e3e5a46a2db0fd677633c9fec6b717d7c2ec3
Joe Tricot <jtricot1@gmail.com>
2017-07-31 17:24:31 -0400

    added user.js model, created auth files in services

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const db = require('../db/config');
+
+const User = {};
+
+User.findByUserName = (email) => {
+  return db.oneOrNone(`
+    SELECT * FROM users
+    WHERE email = $1
+  `, [email]);
+};
+
+User.create = (user) => {
+  return db.one(`
+    INSERT INTO users
+    (email, first_name, last_name, password_digest)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *
+  `, [user.email, user.first_name, user.last_name, user.password_digest]);
+};
+
+module.exports = User;
